2a5f65c506a90b27506517d0afbd59793ae268ee
ArjanGP <arjanpolhaar@gmail.com>
2026-03-06 21:29:19 +0100

    small bug fix

--- hw2/js/tooltips.js
+++ hw2/js/tooltips.js
@@ -1,33 +1,33 @@
-function showTooltip(id) {
-  var tooltip = document.getElementById(id).querySelector(".tooltiptext");
-  tooltip.style.visibility = "visible";
-}
+// function showTooltip(id) {
+//   var tooltip = document.getElementById(id).querySelector(".tooltiptext");
+//   tooltip.style.visibility = "visible";
+// }
 
-function hideTooltip(id) {
-  var tooltip = document.getElementById(id).querySelector(".tooltiptext");
-  tooltip.style.visibility = "hidden";
-}
+// function hideTooltip(id) {
+//   var tooltip = document.getElementById(id).querySelector(".tooltiptext");
+//   tooltip.style.visibility = "hidden";
+// }
 
-function registerEvents() {
-  [
-    "cs2",
-    "ea",
-    "fn",
-    "halo",
-    "pubg",
-    "pubg__mobile",
-    "r6s",
-    "apex",
-    "smash__bros",
-  ].forEach((id) => {
-    const game = document.getElementById(id);
-    game.addEventListener("mouseover", function () {
-      showTooltip(id);
-    });
-    game.addEventListener("mouseout", function () {
-      hideTooltip(id);
-    });
-  });
-}
+// function registerEvents() {
+//   [
+//     "cs2",
+//     "ea",
+//     "fn",
+//     "halo",
+//     "pubg",
+//     "pubg__mobile",
+//     "r6s",
+//     "apex",
+//     "smash__bros",
+//   ].forEach((id) => {
+//     const game = document.getElementById(id);
+//     game.addEventListener("mouseover", function () {
+//       showTooltip(id);
+//     });
+//     game.addEventListener("mouseout", function () {
+//       hideTooltip(id);
+//     });
+//   });
+// }
 
-window.addEventListener("load", registerEvents);
+// window.addEventListener("load", registerEvents);
